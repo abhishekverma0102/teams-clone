c8d51d8436592c0e172e80fbb42c50c25cc22189
Barkkk <86889541+Barkkk@users.noreply.github.com>
2021-07-13 15:17:16 +0530

    Add files via upload

---
+++ login/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCQLvBBdhKDofdLvlDO7KKYUEwRvu_S0wI",
+    authDomain: "login-56ceb.firebaseapp.com",
+    projectId: "login-56ceb",
+    storageBucket: "login-56ceb.appspot.com",
+    messagingSenderId: "415256047870",
+    appId: "1:415256047870:web:243dcbd80c0765c0bcf5af"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+firebase.analytics();
